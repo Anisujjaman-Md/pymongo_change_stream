a33caba615b8bed14057a246d95643f1a504ea77
anisujjaman <anis@surecash.net>
2022-12-01 12:07:50 +0600

    This is 2nd commit

--- changes.py
+++ changes.py
@@ -1,13 +1,15 @@
 import os
-from pymongo import MongoClient
+import pymongo
 from bson.json_util import dumps
 
 if __name__ == '__main__':
-    client = MongoClient(os.environ['CHANGE_STREAM_DB'])
+    client = pymongo.MongoClient(os.environ['CHANGE_STREAM_DB'])
 
     change_stream = client.tkdc.client_data.watch(full_document='updateLookup')
     documement = next(change_stream)
+    print(dumps(documement))
+
     for change in change_stream:
         client.tkdc.client_data_history.insert_one(change)
-        print(dumps(change))
+        print(dumps(change)) 
         print('')
